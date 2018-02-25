03428e63a0394397a59f713c02b48223d2cf3ae3
vargadan <danielvrg@gmail.com>
2018-02-25 19:01:43 +0100

    envsetup

--- pipelines/environment-setup.groovy
+++ pipelines/environment-setup.groovy
@@ -1,19 +1,18 @@
 node {
 
-	def APP_1 = "report-order-manager"
-	def APP_2 = "dummy-report-factory"
-	def APP_3 = "identity-server"
-	def APP_4 = "report-renderer"
-	def APP_5 = "report-repository"
-	def APP_6 = "report-uploader"
+	def APP_ROM = "report-order-manager"
+	def APP_DRF = "dummy-report-factory"
+	def APP_ID = "identity-server"
+	def APP_REN = "report-renderer"
+	def APP_REP = "report-repository"
+	def APP_UP = "report-uploader"
+	def APPS = [APP_ROM, APP_DRF, APP_ID, APP_REN, APP_REP, APP_UP] as String[]
   
   	def DEV_PROJECT = "ctr-dev"
    	def IT_PROJECT = "ctr-it"
    	def PROD_PROJECT = "ctr-prod"
    	
    	def VERSION = "0.0.1"
-   	
-   	def PORT = 8080
    	
    	def verbose = false;
 
@@ -26,63 +25,41 @@ node {
 	}       
    	
    	stage('DEV setup') {
-                rabbitMq(DEV_PROJECT);
-   		envSetup(DEV_PROJECT, APP_1, VERSION, PORT, true)
-   		envSetup(DEV_PROJECT, APP_2, VERSION, PORT, true)
-   		envSetup(DEV_PROJECT, APP_3, VERSION, PORT, true)
-   		envSetup(DEV_PROJECT, APP_4, VERSION, PORT, true)
-   		envSetup(DEV_PROJECT, APP_5, VERSION, PORT, true)
-   		envSetup(DEV_PROJECT, APP_6, VERSION, PORT, true)
+		deleteAll();
+        rabbitMq(DEV_PROJECT);
    	}
    	
    	stage('IT setup') {
-                rabbitMq(IT_PROJECT);
-   		envSetup(IT_PROJECT, APP_1, VERSION, PORT, true)
-   		envSetup(IT_PROJECT, APP_2, VERSION, PORT, true)
-   		envSetup(IT_PROJECT, APP_3, VERSION, PORT, true)
-   		envSetup(IT_PROJECT, APP_4, VERSION, PORT, true)
-   		envSetup(IT_PROJECT, APP_5, VERSION, PORT, true)
-   		envSetup(IT_PROJECT, APP_6, VERSION, PORT, true)
+		deleteAll();
+        rabbitMq(IT_PROJECT);
    	}
 
    	stage('PROD setup') {
-                rabbitMq(PROD_PROJECT);
-   		envSetup(PROD_PROJECT, APP_1, VERSION, PORT, true)
-   		envSetup(PROD_PROJECT, APP_2, VERSION, PORT, true)
-   		envSetup(PROD_PROJECT, APP_3, VERSION, PORT, true)
-   		envSetup(PROD_PROJECT, APP_4, VERSION, PORT, true)
-   		envSetup(PROD_PROJECT, APP_5, VERSION, PORT, true)
-   		envSetup(PROD_PROJECT, APP_6, VERSION, PORT, true)
+		deleteAll();
+        rabbitMq(PROD_PROJECT);
    	}
 
 }
 
+def deleteAll() {
+	sh "oc delete deploymentconfig,service,routes --all -n ${project}"
+}
+
 def rabbitMq(project) {
-    sh "oc delete deploymentconfig,service,routes -l app=rabbitmq -n ${project}"
     sh "oc process -f templates/rabbitmq.yaml | oc create -f - -n ${project}"
 }
 
 
-def envSetup(project, appName, version, port, recreate) {
+def envSetup(project, appNames) {
+	appNames.each {
+		sh "oc process -f templates/app-comp-template.yaml -p APP_NAME=${appName} -p PROJECT_NAME=${project} | oc create -f - -n ${project}"
+	}
+}
 
-	GET_DC_OUT = sh (
-		script: "oc get deploymentconfig -l app=${appName} -n ${project}",
+def hasPods(project) {
+	POD_COUNT = sh (
+		script: "oc get pods -n ${project}",
 		returnStdout: true
 	).trim()
-	
-	echo "GET_DC_OUT : ${GET_DC_OUT}"
-	appExists = GET_DC_OUT.contains(appName)
-	if (appExists && !recreate) {
-		//since the app exists and we dont recreate it we can exit
-		return
-	} else if (appExists && recreate) {
-		//we can delete the app if we want to recreate
-		sh "oc delete deploymentconfig,service,routes -l app=${appName} -n ${project}"
-	}
-		
-	//now we can create the app since it has either been deleted or it did not exist at all
- 	sh "oc new-app ${appName}:latest --allow-missing-images=true --allow-missing-imagestream-tags=true -n ${project}"
-   	sh "oc delete service,routes -l app=${appName} -n ${project}"
-   	sh "oc create service clusterip ${appName} --tcp=${port}:${port} -n ${project}"
-   	sh "oc expose service ${appName} -n ${project}"		
+	return !"No resources found.".equals(POD_COUNT)
 }
